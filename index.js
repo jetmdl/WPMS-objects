7946911dbf4a03f27dfc69d596fbe669f1962d52
jetmdl <91989624+jetmdl@users.noreply.github.com>
2024-01-13 19:16:55 +0000

    created object template

--- index.js
+++ index.js
@@ -1,75 +1,49 @@
-let grid = 0;
-let gridSize = 0;
+let die = {
+    dienumber: "AMS1080-1",
+    orderNumber: "100",
+    partRequired: '1',
+    lipRequired: '1',
+    mandRequired: '1',
+    dieRequired: '1',
+    backerRequired: '1',
+    bolRequired: '1',
 
-function createGrid(anumber) {
-    grid = anumber*anumber;
-    gridSize = 100/anumber;
+    part: {
+        processOne: '-',
+        processTwo: '-',
+        processThree: '-'
+    },
 
-    const el = document.getElementById('main-content-container');
-    while(el.firstChild) el.removeChild(el.firstChild);
+    lip: {
+        processOne: '-',
+        processTwo: '-',
+        processThree: '-'
+    },
 
-    for(let i=0; i<grid; i++){
-        const aDiv = document.createElement('div');
-        aDiv.id = 'sketch-div-' + i;
-        aDiv.className = 'sketch-div';
-        aDiv.style.cssText = 'width: ' + gridSize + '%; height: ' + gridSize + '%;';
+    mand: {
+        processOne: '-',
+        processTwo: '-',
+        processThree: '-'
+    },
 
-        document.getElementById('main-content-container').appendChild(aDiv);
-    }
-    addListeners();
-}
+    die: {
+        processOne: '-',
+        processTwo: '-',
+        processThree: '-'
+    },
 
-function addListeners(){
-    let hovered = document.getElementsByClassName('sketch-div');
-    for (let i = 0; i < hovered.length; i++){
-        hovered[i].addEventListener("mouseover", getHoveredId);
-    }
-}
+    backer: {
+        processOne: '-',
+        processTwo: '-',
+        processThree: '-'
+    },
 
-function getHoveredId(){
-    console.log(this.id);
-    let anId = this.id;
-    let hoveredDiv = document.getElementById(anId);
-    hoveredDiv.style.cssText = 'width: ' + gridSize + '%; height: ' + gridSize + '%; background-color: black';
-}
+    bolster: {
+        processOne: '-',
+        processTwo: '-',
+        processThree: '-'
+    },
+};
 
-function createSideForm(){
-    let new_div = document.createElement('div');
-    let newClass ='searchContainer';
-    new_div.className = newClass;
-    new_div.id = 'searchContainer';
-    document.getElementById('left-content-container').appendChild(new_div);
-
-    //Create title for the form
-    let new_header = document.createElement('h3');
-    let headerClass = 'formHeader';
-    new_header.className = headerClass;
-    document.getElementById('searchContainer').appendChild(new_header);
-
-    //Populate and append the form title. 
-    headerString = 'Enter New Grid Size: '; 
-    new_header.appendChild(document.createTextNode(headerString));
-    // Create line break
-    let br = document.createElement('br');
-    document.getElementById('searchContainer').appendChild(br.cloneNode());
-
-    // Create value field for selected option
-    let valueInput = document.createElement('input');
-    valueInput.setAttribute('type', 'text');
-    valueInput.setAttribute('name', 'input');
-    valueInput.setAttribute('placeholder', 'Grid Size');
-    valueInput.id = 'searchInput';
-
-    document.getElementById('searchContainer').appendChild(valueInput);
-
-    // Create a button and an event listner that passes the values contained in the form to function that updates the correct dictionary. 
-    document.getElementById('searchContainer').appendChild(br.cloneNode());
-    let actionButton = document.createElement('button');
-    actionButton.appendChild(document.createTextNode('Submit'));
-    actionButton.addEventListener('click', function() {createGrid(document.getElementById('searchInput').value)});
-    document.getElementById('searchContainer').appendChild(actionButton);
-}
-
-createGrid(16);
-
-createSideForm();
+console.log(die.dienumber);
+console.log(die.part.processOne);
